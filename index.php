d130d3106358085ecad569d6336a2edb247f1d4e
Mark <marksalet@gmail.com>
2019-02-14 16:58:35 +0100

    Added files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link type="text/css" rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <div class="menu">
+        <div class="button active"><a href="">Home</a></div>
+        <div class="button"><a href="">Profiel</a></div>
+        <div class="button"><a href="">Voortgang</a></div>
+      </div>
+      <div class="profile-outer">
+        <div class="profile-placeholder">
+          <h2 class="profile">Jens</h2>
+          <img class="profile profile-picture" alt"profile picture" src="media/profile-placeholder.png" />
+        </div>
+      </div>
+    </header>
+    <div>
+      <div>
+        <h1>Voortgangsmonitor</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  </body>
+</html>
